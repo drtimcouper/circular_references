8ef90caf91214a335f42eb4e01dba86819d2fc3d
Tim Couper <drtimcouper@gmail.com>
2016-04-09 17:58:32 +0100

    removed *main.py as better shown in tests equivs

--- bad_main.py
+++
@@ -1,3 +0,0 @@
-from circular_references.bad_customer import Customer
-
-print ("doesn't get to here!")
--- fix1_main.py
+++
@@ -1,3 +0,0 @@
-from circular_references.fix1_customer import Customer
-
-print ("gets to here!")
